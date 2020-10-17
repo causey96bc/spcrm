32f20c1710765d5b1149ba43a070b2ab9a0dc78a
Jitesh Doshi <jitesh@office2.spinspire.com>
2020-10-17 12:27:40 -0400

    firebase: add contacts to firestore

--- src/components/firebase/index.ts
+++ src/components/firebase/index.ts
@@ -1,11 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/auth"; //for side effects
+import "firebase/firestore"; //for side effects
 import { readable } from "svelte/store";
 import firebaseConfig from "./config.js";
+
 firebase.initializeApp(firebaseConfig);
+
 export const user = readable(null, (setter) => {
     firebase.auth().onAuthStateChanged((u) => {
         setter(u);
     });
 });
 
+export const db = firebase.firestore();
+
+export function getUserDoc(uid) {
+    return db.doc(`users/${uid}`);
+}
--- src/pages/contacts.svelte
+++ src/pages/contacts.svelte
@@ -1,4 +1,49 @@
-<!-- routify:options title="Conacts" -->
-<!-- routify:options index=2 -->
+<script>
+  import { getUserDoc, user } from "../components/firebase";
+  const CONTACT = { name: "", email: "" };
+  let contact = { ...CONTACT };
+  let contacts = [];
+  const userDoc = getUserDoc($user.uid);
+  const contactsCollection = userDoc.collection("contacts");
+  contactsCollection.onSnapshot((snapshot) => {
+    contacts = snapshot.docs.map((doc) => doc.data());
+  });
+  async function submit() {
+    contactsCollection.add(contact);
+    contact = { ...CONTACT };
+  }
+</script>
 
+<style>
+  form > * {
+    display: block;
+  }
+</style>
+
+<!-- routify:options index=2 -->
+<!-- routify:options title="Conacts" -->
 Your list of contacts.
+<form on:submit|preventDefault={submit}>
+  <input bind:value={contact.name} placeholder="full name" />
+  <input bind:value={contact.email} placeholder="email address" type="email" />
+  <button type="submit">Save</button>
+</form>
+
+<table>
+  <thead>
+    <th>Name</th>
+    <th>Email</th>
+  </thead>
+  <tbody>
+    {#each contacts as c}
+      <tr>
+        <td>{c.name}</td>
+        <td>{c.email}</td>
+      </tr>
+    {:else}
+      <tr>
+        <td colspan="2">You have no contacts. Please add some.</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
