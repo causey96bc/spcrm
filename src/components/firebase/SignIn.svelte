3fed4e4d0102db6c4f86500ddc4d9d14f3ae0a8f
Jitesh Doshi <jitesh@spinspire.com>
2020-10-22 15:09:00 -0400

    feature: send email using gapi

--- src/components/firebase/SignIn.svelte
+++ src/components/firebase/SignIn.svelte
@@ -1,11 +1,13 @@
-<script>
-    import firebase from "firebase/app";
-    import "firebase/auth";
+<script context="module">
+  import firebase from "firebase/app";
+  import "firebase/auth";
 
-    async function signIn() {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        await firebase.auth().signInWithPopup(provider);
-    }
+  export async function signIn() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("https://mail.google.com");
+    const result = await firebase.auth().signInWithPopup(provider);
+    return result;
+  }
 </script>
 
 <button on:click={signIn}>Sign In</button>
--- src/pages/emails.svelte
+++ src/pages/emails.svelte
@@ -1,4 +1,10 @@
+<script>
+  import GMail from "../components/gapi/GMail.svelte";
+</script>
+
 <!-- routify:options title="Emails" -->
 <!-- routify:options index=3 -->
 
 My email interactions.
+
+<GMail />
