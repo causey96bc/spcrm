a9c0c9160dbb41eb392620592252793954dc8942
Jitesh Doshi <jitesh@spinspire.com>
2020-11-05 17:44:07 -0500

    make getUserDoc arg optional

--- src/components/firebase/index.ts
+++ src/components/firebase/index.ts
@@ -1,19 +1,27 @@
 import firebase from "firebase/app";
 import "firebase/auth"; //for side effects
 import "firebase/firestore"; //for side effects
-import { readable } from "svelte/store";
+import { get, readable } from "svelte/store";
 import firebaseConfig from "./config.js";
 
 firebase.initializeApp(firebaseConfig);
 
 export const user = readable(null, (setter) => {
-    firebase.auth().onAuthStateChanged((u) => {
-        setter(u);
-    });
+  firebase.auth().onAuthStateChanged((u) => {
+    setter(u);
+  });
 });
 
 export const db = firebase.firestore();
 
-export function getUserDoc(uid) {
-    return db.doc(`users/${uid}`);
+export function getUserDoc(uid = null) {
+  if (!uid) {
+    const $user = get(user);
+    if ($user) {
+      uid = $user.uid;
+    } else {
+      throw "No uid supplied and user not logged in";
+    }
+  }
+  return db.doc(`users/${uid}`);
 }
