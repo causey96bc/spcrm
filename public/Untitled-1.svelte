b203467ea93dac25d0475b3387dcd062574ef18a
causey904.bc <your_email@real_email.edu>
2020-11-09 16:51:58 -0500

    unittled function

---
+++ public/Untitled-1.svelte
@@ -0,0 +1,12 @@
+$: {
+    console.log("files", files);
+    const [file] = files;
+    if (file) {
+      file.arrayBuffer().then((ab) => {
+        const b64 = btoa(new Uint8Array(ab));
+        console.log("b64", b64);
+      });
+      //   const ref = firebase.storage().ref(`test/${file.name}`);
+      //   ref.put(file);
+    }
+  }
