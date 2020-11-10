1d7f05b8aff5979784b6450793197dc845dfc8f4
David Thomas <your_email@real_email.edu>
2020-11-10 08:35:33 -0500

    Cleaned up dead code

--- src/components/gapi/index.js
+++ src/components/gapi/index.js
@@ -1,5 +1,3 @@
-
-
 import mimetypes from "mime";
 import MIMEAudio from "mime";
 import MIMEImage from "mime";
@@ -9,7 +7,7 @@ import MIMEBase from "mime";
 // console.log("selected files", file);
 import firebaseConfig from "../firebase/config";
 init(firebaseConfig.apiKey, firebaseConfig.clientId);
-console.log("hello")
+console.log("hello");
 export default async function init(apiKey, clientId) {
   const DISCOVERY_DOCS = [
     "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
@@ -38,7 +36,7 @@ ${body}`;
   // The body needs to be base64url encoded.
   const encodedMessage = btoa(message);
   // message.attach(encodedMessage)
-  // let {content_type, encoding} = mimetypes.guess_type(file) 
+  // let {content_type, encoding} = mimetypes.guess_type(file)
   const urlSafeEncodedMessage = encodedMessage
     .replace(/\+/g, "-")
     .replace(/\//g, "_")
@@ -46,47 +44,9 @@ ${body}`;
   const send = await gapi.client.gmail.users.messages.send({
     userId: "me",
     resource: {
-      //   raw: urlSafeEncodedMessage,
-      "payload": {
-
-      }
-    },
-  });
-  console.log("send", send);
-}
-export function sendMailWithUpload(to, subject, body, file) {
-  const gapi = window.gapi;
-  let message = `To: ${to}
-Subject: ${subject}
-Content-type: text/html
-${body}
-file = ${file}`;
-  // The body needs to be base64url encoded.
-  const encodedMessage = btoa(message);
-
-  let content_type, encoding = mimetypes.getType(file)
-
-
-  if (!content_type || encoding) {
-    content_type = 'application/octet-stream'
-    let main_type, sub_type = content_type.split('/', 1);
-
-
-    if (main_type === "text") {
-      window.File(file, "text")
-    }
-  }
-  const urlSafeEncodedMessage = encodedMessage
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
-  const send = gapi.client.gmail.users.messages.send({
-    userId: "me",
-    resource: {
       raw: urlSafeEncodedMessage,
+      payload: {},
     },
   });
   console.log("send", send);
 }
-
-
