b3b32c3c8a39c62dd4650ee1f7722c9ff76b89cc
causey904.bc <your_email@real_email.edu>
2020-11-11 19:42:12 -0500

    creating multiple templates

---
+++ src/pages/templates/[subject].svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Console } from "console";
+
+  import Mustache from "mustache";
+
+  export let templateBody;
+  export let templateSubj;
+  export let body;
+  export let subject;
+  export function loadTemplateBody() {
+    templateBody = `name: {{name}}
+    body: {{body}}`;
+    templateSubj = `Subject: {{Subject}}`;
+    return (body = Mustache.render(templateBody, {
+      name: "{name}",
+      body: "hello thanks for reaching out to our company",
+    }));
+  }
+  export function loadTemplateSubject() {
+    templateSubj = `Subject: {{Subject}}`;
+    subject = Mustache.render(templateSubj, {
+      Subject: "Thank you Email",
+    });
+  }
+  console.log("am i here ", templateSubj);
+  loadTemplateSubject();
+  loadTemplateBody();
+</script>
