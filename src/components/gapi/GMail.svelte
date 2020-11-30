a7328aaf629e1f1115f8b081845efc737d08ef74
David Thomas <your_email@real_email.edu>
2020-11-30 13:43:30 -0500

    added email template styling

--- src/components/gapi/GMail.svelte
+++ src/components/gapi/GMail.svelte
@@ -4,58 +4,68 @@
   export let contacts;
 
   let files = [];
-  let renderedSubject, renderedBody;
-  let subject = "test email to {{contact.name}}";
-  let body = `hello {{contact.name}},
+  let renderedSubject, renderedBody, renderedRecipients, renderedSignature;
+  let recipients = "Recipients: {{contact.email}}";
+  let signature = "";
+  let subject = "follow up";
+  let body = `Hello {{contact.name}},
+    
+I wanted to thank you again for taking the time to chat with me about your 
+app {{contact.details}}. 
+ 
+Can't wait to meet the developing team next week! 
   
-  line1  
-  line2
-  
-  thanks.`;
+Thank you!`;
 
   $: {
     const [contact] = contacts;
+    renderedRecipients = mustache.render(recipients, { contact });
     renderedSubject = mustache.render(subject, { contact });
     renderedBody = mustache.render(body, { contact });
+    renderedSignature = mustache.render(signature, { contact });
   }
 </script>
 
 <style>
   legend {
     text-align: initial;
+    font-size: 28px;
   }
   .email-container {
     display: flex;
     justify-content: center;
     margin-top: 20px;
     border-radius: 5%;
-    width: 500px;
+    width: 600px;
     height: 500px;
-    padding: 10px;
+    padding: 20px;
     box-shadow: 5px 6px 6px 6px #888888;
   }
-
   fieldset > * {
     display: block;
   }
   input {
     width: 255px;
-    margin-bottom: 10px;
     margin: auto;
+    margin-bottom: 12px;
   }
   textarea {
     width: 400px;
+    flex-wrap: wrap;
   }
-
   p {
     margin-top: 12px;
   }
   .sample-email {
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
     margin-top: 20px;
     border-radius: 5%;
-    width: 500px;
+    width: 700px;
     height: 500px;
-    padding: 10px;
+    padding: 20px;
     box-shadow: 5px 6px 6px 6px #888888;
   }
   button {
@@ -68,24 +78,37 @@
     color: white;
     font-size: 16px;
   }
+  pre {
+    margin: 12px;
+    word-wrap: break-word;
+  }
+  .subject_style {
+    border-bottom: 3px solid black;
+  }
 </style>
 
 <div class="email-container">
   <form
     on:submit|preventDefault={() => contacts.forEach((c) =>
-        sendMail(c, subject, body, files)
+        sendMail(c, subject, body, files, signature)
       )}>
     <fieldset>
-      <legend>Send email</legend>
+      <legend>Email Template</legend>
+      <p>Subject</p>
       <input bind:value={subject} placeholder="email 'subject'" required />
+      <p>Message</p>
       <textarea bind:value={body} rows="5" />
+      <p>Signature</p>
+      <input bind:value={signature} placeholder="signature" />
       <input type="file" bind:files multiple />
       <button type="submit">Send</button>
     </fieldset>
   </form>
 </div>
 <div class="sample-email container-fluid">
-  <h3>Sample Email.</h3>
-  <pre>{renderedSubject}</pre>
+  <h3>Email Preview</h3>
+  <pre>{renderedRecipients}</pre>
+  <pre class="subject_style">{renderedSubject}</pre>
   <pre>{renderedBody}</pre>
+  <pre>{renderedSignature}</pre>
 </div>
--- src/pages/qrscanner.svelte
+++ src/pages/qrscanner.svelte
@@ -1,7 +1,60 @@
+<!-- routify:options title="QR Scanner" -->
 <script>
-  import QRScanner from "../components/QRScanner.svelte";
+  import QrScanner from "qr-scanner";
+  import { onMount } from "svelte";
+  let qron = false;
+  $: if (qrscanner) {
+    if (qron) {
+      console.log("started scanning");
+      qrscanner.start();
+    } else {
+      console.log("stopped scanning");
+      qrscanner.stop();
+    }
+  }
+  let video, qrscanner;
+  onMount(() => {
+    qron = true;
+    qrscanner = new QrScanner(video, (result) => {
+      if (result) {
+        console.log("result", result);
+        qron = false;
+      }
+    });
+  });
 </script>
 
-<QRScanner height="100" width="100" opts="...">
-  <div class="placeholder">No cameras loaded!</div>
-</QRScanner>
+<style>
+  .video-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: "Roboto Condensed", sans-serif;
+  }
+  div.qr {
+    position: relative;
+    overflow: hidden;
+  }
+  div.qr video {
+    object-fit: contain;
+    max-width: 800px;
+    max-height: 800px;
+    margin-top: 50px;
+  }
+  button.qr {
+    z-index: 1000;
+    max-width: 100px;
+    text-align: center;
+  }
+</style>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<div class="video-container">
+  <div class="qr">
+    <video bind:this={video} defaultMuted playsinline autoplay preload="auto" />
+  </div>
+  <button class="qr" on:click={() => (qron = !qron)}>
+    {#if qron}Stop{:else}Start{/if}
+    Scanning</button>
+</div>
